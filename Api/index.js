ff93cb59e4732804dbb5ecdb978472a3459a6e66
unknown <osama_magdy0@yahoo.com>
2023-12-23 13:07:33 +0200

    connect mongo database with mongo

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Data Base is connecting");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
