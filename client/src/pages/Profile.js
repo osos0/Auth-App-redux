c59f1a2a172d0cae2530d6383b04c55ef4cd4d92
unknown <osama_magdy0@yahoo.com>
2024-01-11 10:07:53 +0200

    complelet Profile page Ui

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <>
+      <div className="proCont container">
+        <h2>Profile</h2>
+
+        <form>
+          <img
+            src={currentUser.data.profilePic}
+            alt="proPicture"
+            className="picProfile"
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            id="Username"
+            defaultValue={currentUser.data.username}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            defaultValue={currentUser.data.email}
+          />
+          <input type="password" placeholder="Password" id="password" />
+          <button type="button">Update</button>
+        </form>
+        <div className="conOfDeleteandLogout">
+          <span>Delete Account</span>
+          <span>Log Out</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
